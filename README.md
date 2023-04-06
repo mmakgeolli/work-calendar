83ee787aae3ad775c664a8ac41434d930ceb437a
makgeolli <mordredphantom@gmail.com>
2023-04-06 14:38:35 -0500

    added to javascript code to create a working version of calendar, hours for timeblocks 9 AM - 5 PM were added as well, each timeblock has a text box in which text can be saved to local storage by clicking the save button. The colors of each timeblock still need to be fixed to meet the past, present, and future ID's

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# work-calendar
-A calendar that notes tasks!
+# work-scheduler
+
+A scheduler that notes tasks!
+
+This work scheduler meets the following criteria:
+
+- This is a daily planner that can be used to create a schedule during work hours (9 AM - 5 PM)
+- The current date is displayed on the top of the page
+- The timeblocks are presented for each business hour (9 AM - 5 PM)
+- The timeblocks are color coded to indicate past, present, or future
+- The events can be saved onto each timeblock after clicking the "SAVE" button
+- The text that is saved onto each timeblock is saved in the local storage
+- When the page is refreshed, the text (event) saved persists
+
+The following is a screenshot of what the page should look like when working properly:
--- script.js
+++ script.js
@@ -1,23 +1,67 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Get the current date and time using Day.js library
+var currentDate = dayjs().format('dddd, MMMM D');
+var currentTime = dayjs().format('H');
+
+// Display the current date at the top of the calendar
+$("#currentDay").text(currentDate);
+
+// Create an array of timeblocks for standard business hours
+var timeBlocks = [
+  { hour: "9", time: "9AM", },
+  { hour: "10", time: "10AM", },
+  { hour: "11", time: "11AM", },
+  { hour: "12", time: "12PM", },
+  { hour: "13", time: "1PM", },
+  { hour: "14", time: "2PM", },
+  { hour: "15", time: "3PM", },
+  { hour: "16", time: "4PM", },
+  { hour: "17", time: "5PM", },
+];
+
+// Loop through the timeBlocks array and dynamically create timeblock elements
+for (var i = 0; i < timeBlocks.length; i++) {
+  var timeBlockEl = $("<div>").attr("id", "hour-" + timeBlocks[i].hour).addClass("row time-block");
+
+  var hourEl = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(timeBlocks[i].time);
+  var descriptionEl = $("<textarea>").addClass("col-8 col-md-10 description");
+
+  // Set the timeblock's color based on past, present, or future
+  if (parseInt(timeBlocks[i].hour) < parseInt(currentTime)) {
+    descriptionEl.addClass("past");
+  } else if (parseInt(timeBlocks[i].hour) === parseInt(currentTime)) {
+    descriptionEl.addClass("present");
+  } else {
+    descriptionEl.addClass("future");
+  }
+
+  var saveBtnEl = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+  var saveIconEl = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true");
+
+  // Append elements to the timeBlockEl
+  saveBtnEl.append(saveIconEl);
+  timeBlockEl.append(hourEl, descriptionEl, saveBtnEl);
+
+  // Append the timeblock element to the container
+  $(".container-lg").append(timeBlockEl);
+}
+
+// Load saved events from local storage
+function loadSavedEvents() {
+  for (var i = 0; i < timeBlocks.length; i++) {
+    var eventText = localStorage.getItem("event-" + timeBlocks[i].hour);
+    if (eventText) {
+      $("#hour-" + timeBlocks[i].hour + " .description").val(eventText);
+    }
+  }
+}
+
+// Save the event when the save button is clicked
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+  var hour = $(this).parent().attr("id").replace("hour-", "");
+  var eventText = $(this).siblings(".description").val();
+  localStorage.setItem("event-" + hour, eventText);
 });
+
+// Call the loadSavedEvents function to load any saved events from local storage
+loadSavedEvents();
